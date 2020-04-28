fcd01ee5b2ca5fd6b7363d53dc5c3661b218de72
akcybertek <64118001+akcybertek@users.noreply.github.com>
2020-04-28 15:52:09 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -5,6 +5,10 @@ public class AllGood {
     public static void main(String[] args) {
 
         // I hope everything is GOOD.
+        
+        //I hope everything will go well
+        
+        //It will take time
 
     }
 }
